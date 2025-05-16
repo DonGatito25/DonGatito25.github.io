19834e71d474696ad0f3459a1085117aa82cff57
Daniel Cabrera Garcia <dcabre290@west-mec.org>
2025-05-16 15:48:34 -0700

    e2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // Use root path for user sites 
-  build: { outDir: 'dist', assetsDir: 'assets', rollupOptions: { output: { manualChunks: undefined, entryFileNames: 'assets/[name].[hash].js', chunkFileNames: 'assets/[name].[hash].js', assetFileNames: 'assets/[name].[hash].[ext]' } }}
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
